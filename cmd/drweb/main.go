ea9214499d6a808e96e4df46e4dedd004b4f348e
twonegatives <whitewhiteheaven@gmail.com>
2018-07-25 01:07:31 +0300

    - Config made to be a singletone - Cfg, Storage variables are not global variables anymore

--- cmd/drweb/main.go
+++ cmd/drweb/main.go
@@ -17,35 +17,36 @@ import (
 	"github.com/twonegatives/drweb_challenge/pkg/storages"
 )
 
-// NOTE: we use double folder nesting here in order to overcome
-// issue with too much files lying in a single folder.
-// in case we're ok with a single nesting Levels param may be changed
-var pathgen = pathgenerators.NestedGenerator{
-	Levels:       2,
-	FolderLength: 2,
-	BasePath:     ".",
-}
-
-var storage = storages.FileSystemStorage{
-	FileMode:          0700,
-	FilePathGenerator: &pathgen,
-}
-
-// NOTE: we use some leading file bytes to generate hash and append unix time to it
-// so that it is not required to read the whole file to generate its filename.
-// in case we really want to build filename based on the whole file contents
-// there is another generator for exactly this purpose: namegenerators.SHA256
-var filenamegenerator = namegenerators.LeadingSHA256WithUnixTime{LeadingSize: 50}
-
 func main() {
-	cfg := config.NewConfig()
+	cfg := config.GetConfig()
+
+	// NOTE: we use double folder nesting here in order to overcome
+	// issue with too much files lying in a single folder.
+	// in case we're ok with a single nesting Levels param may be changed
+	var pathgen = pathgenerators.NestedGenerator{
+		Levels:       2,
+		FolderLength: 2,
+		BasePath:     ".",
+	}
+
+	var storage = storages.FileSystemStorage{
+		FileMode:          0700,
+		FilePathGenerator: &pathgen,
+	}
+
+	// NOTE: we use some leading file bytes to generate hash and append unix time to it
+	// so that it is not required to read the whole file to generate its filename.
+	// in case we really want to build filename based on the whole file contents
+	// there is another generator for exactly this purpose: namegenerators.SHA256
+	var filenamegenerator = namegenerators.LeadingSHA256WithUnixTime{LeadingSize: 50}
 
 	router := mux.NewRouter()
 	startSaveCbk := callbacks.LogCallback{Content: "Started to save a file"}
 	finishSaveCbk := callbacks.LogCallback{Content: "Finished file saving process"}
+	createFile := createFileHandler(&storage, &filenamegenerator)
 	router.HandleFunc("/files", withCallbacks(createFile, &startSaveCbk, &finishSaveCbk)).Methods("POST")
-	router.HandleFunc("/files/{hashstring}", retrieveFile).Methods("GET")
-	router.HandleFunc("/files/{hashstring}", deleteFile).Methods("DELETE")
+	router.HandleFunc("/files/{hashstring}", retrieveFileHandler(&storage)).Methods("GET")
+	router.HandleFunc("/files/{hashstring}", deleteFileHandler(&storage)).Methods("DELETE")
 
 	srv := &http.Server{
 		Handler:      router,
@@ -65,49 +66,55 @@ func withCallbacks(handler func(http.ResponseWriter, *http.Request), before drwe
 	}
 }
 
-func createFile(w http.ResponseWriter, r *http.Request) {
-	var formFile multipart.File
-	var formFileHeader *multipart.FileHeader
-	var file *drweb.File
-	var err error
+func createFileHandler(storage drweb.Storage, filenamegenerator drweb.FileNameGenerator) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var formFile multipart.File
+		var formFileHeader *multipart.FileHeader
+		var file *drweb.File
+		var err error
 
-	if formFile, formFileHeader, err = r.FormFile("file"); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+		if formFile, formFileHeader, err = r.FormFile("file"); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-	if file, err = drweb.NewFile(formFile, formFileHeader.Header, &storage, &filenamegenerator); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+		if file, err = drweb.NewFile(formFile, formFileHeader.Header, storage, filenamegenerator); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-	if _, err = storage.Save(file); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+		if _, err = storage.Save(file); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(file)
-}
-
-func retrieveFile(w http.ResponseWriter, req *http.Request) {
-	var file *drweb.File
-	var err error
-
-	vars := mux.Vars(req)
-	filename := vars["hashstring"]
-
-	if file, err = storage.Load(filename); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	if _, err = io.Copy(w, file.Body); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(file)
 	}
 }
 
-func deleteFile(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+func retrieveFileHandler(storage drweb.Storage) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		var file *drweb.File
+		var err error
 
-	if err := storage.Delete(vars["hashstring"]); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		vars := mux.Vars(req)
+		filename := vars["hashstring"]
+
+		if file, err = storage.Load(filename); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		if _, err = io.Copy(w, file.Body); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func deleteFileHandler(storage drweb.Storage) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
+		if err := storage.Delete(vars["hashstring"]); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,15 +1,34 @@
 package config
 
 import (
+	"sync"
 	"time"
 
 	"github.com/spf13/viper"
 )
 
+var cfg *viper.Viper
+var once sync.Once
+
+func GetConfig() *viper.Viper {
+	once.Do(func() {
+		defaults := getDefaults()
+		cfg := viper.New()
+		cfg.SetDefault("LISTEN", defaults.Listen)
+		cfg.SetDefault("WRITE_TIMEOUT", defaults.WriteTimeout)
+		cfg.SetDefault("READ_TIMEOUT", defaults.ReadTimeout)
+		cfg.SetDefault("MAX_FILE_SIZE", defaults.MaxFileSize)
+		cfg.AutomaticEnv()
+	})
+
+	return cfg
+}
+
 type configDefaults struct {
 	Listen       string
 	ReadTimeout  time.Duration
 	WriteTimeout time.Duration
+	MaxFileSize  int
 }
 
 func getDefaults() *configDefaults {
@@ -17,15 +36,6 @@ func getDefaults() *configDefaults {
 		Listen:       ":80",
 		WriteTimeout: 15,
 		ReadTimeout:  15,
+		MaxFileSize:  3 * 1000 * 1000,
 	}
-}
-
-func NewConfig() *viper.Viper {
-	defaults := getDefaults()
-	cfg := viper.New()
-	cfg.SetDefault("LISTEN", defaults.Listen)
-	cfg.SetDefault("WRITE_TIMEOUT", defaults.WriteTimeout)
-	cfg.SetDefault("READ_TIMEOUT", defaults.ReadTimeout)
-	cfg.AutomaticEnv()
-	return cfg
 }
